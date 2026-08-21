31e00056ceac5d982ff3536fd198a294484f3dae
adahyli <adilahyanali@gmail.com>
2026-08-21 18:11:23 +0100

    Fix accidental nested Git repository

---
+++ exercises/hello_world/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
+    std::cout << "Hello, World!\n";
+    std::cout << "Hello, github!\nHere we go!\n";
+    return 0;
+}  
